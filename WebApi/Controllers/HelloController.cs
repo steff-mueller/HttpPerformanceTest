e82996dbccf38a5d9491d8e9123d0cbddd61a3a7
Steffen Müller <arxisos@googlemail.com>
2012-09-27 22:21:07 +0200

    added WebApi project

---
+++ WebApi/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HttpPerformanceTest.Controllers
+{
+    public class HelloController : ApiController
+    {
+        // GET api/hello
+        public string Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ WebApi/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace HttpPerformanceTest
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
